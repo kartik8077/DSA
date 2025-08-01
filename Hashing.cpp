cf638b3a31519f4aed8655f6eaf472fd9e2a682f
kartik8077 <kartikbhardwaj0619@gmail.com>
2025-08-01 11:37:25 +0530

    this is the code through which we had to input the array

---
+++ Hashing.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    // input an array
+    int n;
+    cout<<"enter the size of array:";
+    cin>>n;
+    int arr[n]={0};
+    cout<<"enter the array:";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];    
+    }
