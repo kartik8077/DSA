d01b0eb3e5e0945ebdb51f73789ea88afbfac251
kartik8077 <kartikbhardwaj0619@gmail.com>
2025-08-01 11:56:42 +0530

    this is a simple skeleton for the code

---
+++ Highest_Occurring_Element_in_Array.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    // input an array
+
+
+    // precompute
+
+
+    // find the highest frequency occuring element
+
+    
+    
+    return 0;
+}
