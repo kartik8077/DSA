87b27ac08614cf87ee218d795d999438efa9f78e
kartik8077 <kartikbhardwaj0619@gmail.com>
2025-08-02 09:21:43 +0530

    this is boilerplate for the bubble sort

---
+++ sorting/bubble_sort.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void bubble_sort(int arr[],int n){
+    
+}
+
+int main(){
+    int arr[6]={23,45,65,2,6,78};
+    int n=6;
+    bubble_sort(arr,n);
+    return 0;
+}
