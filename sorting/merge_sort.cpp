18480582bd090b9d02e252d06e0e9c78a1f79942
kartik8077 <kartikbhardwaj0619@gmail.com>
2025-08-02 09:43:01 +0530

    this the boilerplate template for the merge sort

---
+++ sorting/merge_sort.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void mergesort(int arr[],int n){
+
+}
+
+int main(){
+    int arr[6]={23,45,2,66,43,6};
+    int n=6;
+    mergesort(arr,n);
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
