092709fb18adc05639ab87f330d98f21d4559c71
kartik8077 <kartikbhardwaj0619@gmail.com>
2025-08-02 13:12:03 +0530

    this the dividing step

--- sorting/merge_sort.cpp
+++ sorting/merge_sort.cpp
@@ -1,14 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void mergesort(int arr[],int n){
-
+void mergesort(int arr[],int st,int end){
+    int mid=st+(end-st)/2;
+    if(st>=end){
+        return;
+    }
+    mergesort(arr,st,mid);
+    mergesort(arr,mid+1,end);
 }
+
+
 
 int main(){
     int arr[6]={23,45,2,66,43,6};
     int n=6;
-    mergesort(arr,n);
+    mergesort(arr,0,n-1);
     for(int i=0;i<n;i++){
         cout<<arr[i]<<" ";
     }
