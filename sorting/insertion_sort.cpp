18a0b4a3385f244f41424a52a48f87cc6e7b77b8
kartik8077 <kartikbhardwaj0619@gmail.com>
2025-08-03 17:46:26 +0530

    this the full code of the _INSERTION SORT_

--- sorting/insertion_sort.cpp
+++ sorting/insertion_sort.cpp
@@ -2,13 +2,21 @@
 using namespace std;
 
 void insertion(int arr[],int n){
-   
+    for(int i=1;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(arr[i]<arr[j]){
+                swap(arr[i],arr[j]);
+            }
+        }
+   }
 }
 
 int main(){
-    int arr[6]={34,56,44,6,1,23};
-    int n=6;
+    int arr[8]={6,8,1,4,5,3,7,2};
+    int n=8;
     insertion(arr,n);
-
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
     return 0;
 }
