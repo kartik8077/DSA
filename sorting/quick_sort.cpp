14b717b80cc37389223a734dae07d187200ee830
kartik8077 <kartikbhardwaj0619@gmail.com>
2025-08-03 16:42:13 +0530

    this is the boiler plate code for the quick sort

--- sorting/quick_sort.cpp
+++ sorting/quick_sort.cpp
@@ -1,14 +1,14 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void insertion(int arr[],int n){
+void quicksort(int arr[],int n){
    
 }
 
 int main(){
     int arr[6]={34,56,44,6,1,23};
     int n=6;
-    insertion(arr,n);
+    quicksort(arr,n);
 
     return 0;
 }
