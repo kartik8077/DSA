1d466920133b5dda2f52346f9424677a19a4c7c6
kartik8077 <kartikbhardwaj0619@gmail.com>
2025-08-03 16:39:27 +0530

    this is just a practise

--- sorting/insertion_sort.cpp
+++ sorting/insertion_sort.cpp
@@ -3,12 +3,16 @@ using namespace std;
 
 void insertion(int arr[],int n){
     
+
+
+
+    
 }
 
 int main(){
     int arr[6]={34,56,44,6,1,23};
     int n=6;
     insertion(arr,n);
-    
+
     return 0;
 }
