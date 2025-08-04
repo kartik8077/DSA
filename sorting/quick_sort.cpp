a765e9b3c367dfb7287ba778697d8be332e8ca0c
kartik8077 <kartikbhardwaj0619@gmail.com>
2025-08-04 17:28:16 +0530

    this is code for _quick sort_

--- sorting/quick_sort.cpp
+++ sorting/quick_sort.cpp
@@ -1,46 +1,58 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int partion(int arr[],int low,int high){
-    int pivot=arr[low];
-    int i=low;
-    int j=high;
-    while (i<j)
-    {
-        while (arr[i]<=arr[pivot]&& i<=high)
-        {
+// Partition function using Hoare’s method (pivot = first element)
+int partition(int arr[], int low, int high) {
+    int pivot = arr[low];  // Choosing the first element as pivot
+    int i = low;
+    int j = high;
+
+    while (i < j) {
+        // Move i to the right until an element > pivot is found
+        while (arr[i] <= pivot && i <= high - 1) {
             i++;
         }
-        while (arr[j]>arr[pivot]&&j>=low)
-        {
+
+        // Move j to the left until an element <= pivot is found
+        while (arr[j] > pivot && j >= low + 1) {
             j--;
         }
-        if (i<j){
-            swap(arr[i],arr[j]);
+
+        // If i is still less than j, swap arr[i] and arr[j]
+        if (i < j) {
+            swap(arr[i], arr[j]);
         }
-        
     }
-    
+
+    // Place the pivot in its correct position
+    swap(arr[low], arr[j]);
+
+    return j;  // Return the final position of the pivot
 }
 
-// quick sort
-void quicksort(int arr[],int low, int high){
-    if (low<high){
-        int pindex=partion(arr,low,high);
-        quicksort(arr,low,pindex-1);
-        quicksort(arr,pindex+1,high);
-        
+// Quick Sort function (recursive)
+void quicksort(int arr[], int low, int high) {
+    if (low < high) {
+        // Partition the array and get pivot index
+        int pindex = partition(arr, low, high);
+
+        // Recursively sort left and right subarrays
+        quicksort(arr, low, pindex - 1);
+        quicksort(arr, pindex + 1, high);
     }
-   
 }
 
+int main() {
+    int arr[6] = {34, 56, 44, 6, 1, 23};
+    int n = 6;
 
-int main(){
-    int arr[6]={34,56,44,6,1,23};
-    int n=6;
-    quicksort(arr,0,n-1);
-    for(int i=0 ;i<n;i++){
-        cout<<arr[i]<<" ";
+    // Call quicksort with correct indices
+    quicksort(arr, 0, n - 1);
+
+    // Print the sorted array
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
     }
+
     return 0;
 }
