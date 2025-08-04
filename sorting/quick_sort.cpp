8a1ad8b5b3c39872055ff3eb67536c99feda622e
kartik8077 <kartikbhardwaj0619@gmail.com>
2025-08-04 17:25:56 +0530

    this is just an idea

--- sorting/quick_sort.cpp
+++ sorting/quick_sort.cpp
@@ -1,16 +1,46 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int partion(int arr[],int low,int high){
+    int pivot=arr[low];
+    int i=low;
+    int j=high;
+    while (i<j)
+    {
+        while (arr[i]<=arr[pivot]&& i<=high)
+        {
+            i++;
+        }
+        while (arr[j]>arr[pivot]&&j>=low)
+        {
+            j--;
+        }
+        if (i<j){
+            swap(arr[i],arr[j]);
+        }
+        
+    }
+    
+}
 
 // quick sort
-void quicksort(int arr[],int n){
+void quicksort(int arr[],int low, int high){
+    if (low<high){
+        int pindex=partion(arr,low,high);
+        quicksort(arr,low,pindex-1);
+        quicksort(arr,pindex+1,high);
+        
+    }
    
 }
+
 
 int main(){
     int arr[6]={34,56,44,6,1,23};
     int n=6;
-    quicksort(arr,n);
-
+    quicksort(arr,0,n-1);
+    for(int i=0 ;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
     return 0;
 }
