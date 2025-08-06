86d0ab3091f843dabfe518cc0f0b42721a1e0050
kartik8077 <kartikbhardwaj0619@gmail.com>
2025-08-06 06:54:50 +0530

    this the code to find the largest elment in the array

--- array/p1_largest_element_in_array.cpp
+++ array/p1_largest_element_in_array.cpp
@@ -1,6 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int largestelement(int arr[],int n){
+    int largest=arr[0];
+    for(int i=0 ;i<n;i++){
+        if(arr[i]>largest){
+            largest=arr[i];
+        }
+    }
+    return largest;
+}
+
 int main(){
+    int arr[]={1,3,2,45,4};
+    int n=5;
+    cout<<largestelement(arr,n);
     return 0;
 }
