f093120dc0c85df427740f8e8f7e98696fe94a5d
kartik8077 <kartikbhardwaj0619@gmail.com>
2025-08-10 10:31:00 +0530

    this code's t.c is O(n)

--- array/p2_check_if_array_is_sorted_and_rotated.cpp
+++ array/p2_check_if_array_is_sorted_and_rotated.cpp
@@ -3,18 +3,16 @@ using namespace std;
 
 int check(int arr[],int n){
     for(int i=1;i<n;i++){
-        for(int j=i+1;j<n;j++){
-            if(arr[j]<arr[i]){
+            if(arr[i]<arr[i+1]){
                 return false;
             }
         }
-    }
     return true;
 
 }
 int main(){
     int arr[6]={1,2,2,5,3,4};
     int n=6;
-    cout<<check(arr,n);
+    cout<<boolalpha<<check(arr,n);
     return 0;
 }
