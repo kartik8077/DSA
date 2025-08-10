0ddb143b91565639ab9dfd4a0e99479bb1ee0f29
kartik8077 <kartikbhardwaj0619@gmail.com>
2025-08-10 10:29:26 +0530

    this code's logic is correct but t.c is O(nsq.)

--- array/p2_check_if_array_is_sorted_and_rotated.cpp
+++ array/p2_check_if_array_is_sorted_and_rotated.cpp
@@ -1,10 +1,20 @@
 #include<iostream>
 using namespace std;
 
-int check(){
+int check(int arr[],int n){
+    for(int i=1;i<n;i++){
+        for(int j=i+1;j<n;j++){
+            if(arr[j]<arr[i]){
+                return false;
+            }
+        }
+    }
+    return true;
 
 }
 int main(){
-    
+    int arr[6]={1,2,2,5,3,4};
+    int n=6;
+    cout<<check(arr,n);
     return 0;
 }
