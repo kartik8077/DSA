af29fc8caa0c1b497dd72f0a80ba4a8770df1e5d
kartik8077 <kartikbhardwaj0619@gmail.com>
2025-08-11 08:58:14 +0530

    this code rotates an array by k places right

---
+++ array/p6_rotate_array_right.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+void rotate(int arr[],int n,int q){
+    while (q--)
+    {
+        int temp=arr[n-1];
+        for(int i=n-2;i>=0;i--){
+            arr[i+1]=arr[i];
+        }
+        arr[0]=temp;
+    }
+    
+}
+
+int main(){
+    int arr[7]={1,2,3,4,5,6,7};
+    int n=7,q;
+    cout<<"enter the number :";
+    cin>>q;
+    rotate(arr,n,q);
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";  
+    }
+    return 0;
+}
